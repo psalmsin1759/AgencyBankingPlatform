9a5a0b4cb436bb66f6483102dffb4cb1cd6d8a59
psalmsin1759 <samson_ude@yahoo.com>
2023-11-03 13:59:42 +0100

    admiin

---
+++ app/Models/Admin.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'role',
+        'password',
+        'status',
+    ];
+}
